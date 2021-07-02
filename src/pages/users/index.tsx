a3764958f7e6b44a1a6d3fd0689622880496b4d4
Renato Cintra <renatocinttra@gmail.com>
2021-07-02 04:49:08 -0300

    Building the application (Hook List and Pagination)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,41 +1,34 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, ButtonGroup, IconButton, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import Link from "next/link";
-import { useEffect } from "react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createAt: new Date(user.createAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                })
-            };
-        });
-
-        return users;
-    }, {
-        staleTime: 1000 * 5, // 5 seconds
-    })
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page)
     
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
     
+
+    async function handlePrefetchUser(userId: number) {
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data;
+        }, {
+            staleTime: 1000 * 60 * 10, // 10 minutes
+        })
+    }
 
     return (
         <Box>
@@ -45,9 +38,13 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
 
-                        <Link href="/users/create" passHref>
+                        </Heading>
+
+                        <NextLink href="/users/create" passHref>
                             <Button 
                                 as="a" 
                                 size="sm" 
@@ -57,7 +54,7 @@ export default function UserList() {
                             >
                                 Criar novo
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
 
                     { isLoading ? (
@@ -82,19 +79,21 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
-                                                    <Checkbox colorScheme="purple" />
+                                                    <Checkbox colorScheme="pink" />
                                                 </Td>
                                                 <Td>
                                                     <Box>
-                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                        </Link>
                                                         <Text fontWeight="bold">{user.email}</Text>
                                                     </Box>
                                                 </Td>
-                                                { isWideVersion && <Td>{user.createAt}</Td> }
+                                                { isWideVersion && <Td>{user.createdAt}</Td> }
                                                 <Td>
                                                     <Button
                                                         as="a"
@@ -112,7 +111,11 @@ export default function UserList() {
                                 </Tbody>
                         </Table>
 
-                            <Pagination />
+                            <Pagination
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
                         </>
                     )}
 
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,10 +1,10 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import faker from 'faker'
 
 type User = {
     name: string;
     email: string;
-    create_at: string;
+    created_at: string;
 }
 
 export function makeServer() {
@@ -21,22 +21,40 @@ export function makeServer() {
                 email() {
                     return faker.internet.email().toLowerCase();
                 },
-                createAt() {
-                    return faker.date.recent(10);
+                createdAt() {
+                    return faker.date.recent(200);
                 },
 
             })
         },
         
         seeds(server) {
-            server.createList('user', 10)
+            server.createList('user', 200)
         },
 
         routes() {
             this.namespace = 'api';
             this.timing = 750;
 
-            this.get('/users');
+            this.get('/users', function (schema, request) {
+                const { page = 1, per_page = 10 } = request.queryParams
+
+                const total = schema.all('user').length
+
+                const pageStart = (Number(page) - 1) * Number(per_page);
+                const pageEnd = pageStart + Number(per_page);
+
+                const users = this.serialize(schema.all('user'))
+                .users.slice(pageStart, pageEnd)
+
+                return new Response(
+                    200,
+                    { 'x-total-count': String(total) },
+                    { users }
+                )
+            });
+
+            this.get('/users/:id');
             this.post('/users');
 
             this.namespace = '';
